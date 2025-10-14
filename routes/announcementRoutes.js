adfc9a3faa0c617682b48719e09e398e700237b3
Shivam Jha <jha24978@gmail.com>
2025-10-14 12:44:48 +0530

    chnage in some logic uploading the image and document via cloudanry

--- routes/announcementRoutes.js
+++ routes/announcementRoutes.js
@@ -5,11 +5,11 @@ import { authenticateToken } from "../middleware/auth.js";
 import { uploadSingle, handleUploadError } from "../middleware/upload.js";
 import { uploadAny } from "../middleware/upload.js";
 
-import { uploadAnnouncementImage, uploadAnnouncementDocument } from "../controller/announcementController.js";
+// import { uploadAnnouncementImage, uploadAnnouncementDocument } from "../controller/announcementController.js";
 router.post('/createAnnouncement', authenticateToken, createAnnouncement);
 router.get('/getAnnouncement', authenticateToken, getAnnouncement);
 router.put('/updateAnnouncement/:id', authenticateToken, updateAnnouncement);
 router.delete('/deleteAnnouncement/:id', authenticateToken, deleteAnnouncement);
-router.post('/announcement/:id/image', authenticateToken, uploadSingle, handleUploadError, uploadAnnouncementImage);
-router.post('/announcement/:id/document', authenticateToken, uploadAny, handleUploadError, uploadAnnouncementDocument);
+// router.post('/announcement/:id/image', authenticateToken, uploadSingle, handleUploadError, uploadAnnouncementImage);
+// router.post('/announcement/:id/document', authenticateToken, uploadAny, handleUploadError, uploadAnnouncementDocument);
 export default router;
